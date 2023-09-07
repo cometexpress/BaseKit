2b36acb96706e5f94dcd6dcfc591b83acb551c30
hyeseong <hyeseong7848@gmail.com>
2023-09-07 18:45:55 +0900

    Initial Commit

---
+++ BaseKit/BaseKit.h
@@ -0,0 +1,18 @@
+//
+//  BaseKit.h
+//  BaseKit
+//
+//  Created by 장혜성 on 2023/09/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseKit.
+FOUNDATION_EXPORT double BaseKitVersionNumber;
+
+//! Project version string for BaseKit.
+FOUNDATION_EXPORT const unsigned char BaseKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseKit/PublicHeader.h>
+
+
